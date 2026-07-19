2cf1b794fe0d1df362f12d3ca57d6a8dd7eca94e
aurore204 <mariane-aurore.njimegne-kouosseu@epitech.eu>
2026-07-19 21:57:58 +0200

    feat(reaction): ajouter les routes pour la gestion des réactions publiques et protégées

--- server/src/routes/mod.rs
+++ server/src/routes/mod.rs
@@ -3,6 +3,7 @@ use axum::{middleware, routing::get, Router};
 mod auth_routes;
 mod incident_routes;
 mod team_routes;
+mod reaction_routes;
 
 use crate::middleware::auth_middleware::require_auth;
 use crate::state::AppState;
@@ -10,12 +11,15 @@ use crate::websocket::handler::ws_handler;
 
 pub fn create_router(state: AppState) -> Router {
     let public_routes = Router::new()
-        .merge(auth_routes::public_routes());
+        .merge(auth_routes::public_routes())
+        .merge(reaction_routes::public_reaction_routes());
+
 
     let protected_routes = Router::new()
         .merge(auth_routes::protected_routes())
         .merge(team_routes::team_routes())
         .merge(incident_routes::incident_routes())
+        .merge(reaction_routes::protected_reaction_routes())
         .route("/ws", get(ws_handler))
         .layer(middleware::from_fn_with_state(
             state.pool.clone(),
