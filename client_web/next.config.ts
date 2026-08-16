05d660fd44ce3f03e6b1916417a50a320d59eb53
aurore204 <mariane-aurore.njimegne-kouosseu@epitech.eu>
2026-08-16 09:44:36 +0200

    delete de fichiers inutilisés et mise à jour de la configuration Next.js pour l'export statique

--- client_web/next.config.ts
+++ client_web/next.config.ts
@@ -3,6 +3,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // l'optimisation d'images Next.js a besoin d'un serveur, incompatible avec l'export statique
+  },
+};
 
 export default withNextIntl(nextConfig);
