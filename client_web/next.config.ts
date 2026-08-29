6647238c1334907be8088450162af8a491160d01
aurore <aurorenjimegne@gmail.com>
2026-08-29 13:18:24 +0200

    feat: ajout de la prise en charge des fichiers de localisation et mise à jour de la configuration du serveur

--- client_web/next.config.ts
+++ client_web/next.config.ts
@@ -4,10 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // l'optimisation d'images Next.js a besoin d'un serveur, incompatible avec l'export statique
-  },
+  output: "standalone",
 };
 
 export default withNextIntl(nextConfig);
